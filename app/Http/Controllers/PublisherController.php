46e8a9c147edd62085bfb8cfbac60dfc34b4bb6f
Sanjay <sanjay@example.com>
2026-03-11 16:43:12 +0530

    added publisher logo to publisher table

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use App\Models\Publisher;
+use Illuminate\Support\Str;
 
 class PublisherController extends Controller
 {
@@ -21,10 +22,19 @@ class PublisherController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'publisher_logo' => 'required|image|mimes:png,jpg,jpeg|max:2048',
             'publisher_name' => 'required'
         ]);
 
+        $publisher_logoName = time().'_'.$request->publisher_logo->getClientOriginalName();
+
+        $request->publisher_logo->move(
+            public_path('uploads/publisher_logo'),
+            $publisher_logoName
+        );
+
         Publisher::create([
+            'publisher_logo' => $publisher_logoName,
             'publisher_name' => $request->publisher_name
         ]);
 
@@ -36,14 +46,32 @@ class PublisherController extends Controller
         $publisher = Publisher::findOrFail($id);
 
         $request->validate([
+            'publisher_logo' => 'nullable|image|mimes:png,jpg,jpeg|max:2048',
             'publisher_name' => 'required'
         ]);
 
-        $publisher->update([
+        $data = [
             'publisher_name' => $request->publisher_name
-        ]);
+        ];
+
+        if ($request->hasFile('publisher_logo')) {
+
+            if ($publisher->publisher_logo && file_exists(public_path('uploads/publisher_logo/'.$publisher->publisher_logo))) {
+                unlink(public_path('uploads/publisher_logo/'.$publisher->publisher_logo));
+            }
+
+            $publisher_logoName = Str::uuid().'.'.$request->publisher_logo->extension();
+
+            $request->publisher_logo->move(
+                public_path('uploads/publisher_logo'),
+                $publisher_logoName
+            );
+
+            $data['publisher_logo'] = $publisher_logoName;
+        }
+
+        $publisher->update($data);
 
         return redirect()->back();
     }
-    
 }
